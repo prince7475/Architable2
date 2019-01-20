6f044dbe5cdd467cf3949945ddc3c56dc4f2220a
Prince Donkor <prince.donkor74@gmail.com>
2019-01-20 12:34:08 -0600

    hosted website

---
+++ y/precache-manifest.136f42e4a133712c6df9cdffdcefcf40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22e18a0be2d3b663e748",
+    "url": "/static/js/main.22e18a0b.chunk.js"
+  },
+  {
+    "revision": "59fbc7da132b07324045",
+    "url": "/static/js/1.59fbc7da.chunk.js"
+  },
+  {
+    "revision": "22e18a0be2d3b663e748",
+    "url": "/static/css/main.5e81f418.chunk.css"
+  },
+  {
+    "revision": "7c0f65791be9bde6fbe15ebe731d81d3",
+    "url": "/index.html"
+  }
+];
