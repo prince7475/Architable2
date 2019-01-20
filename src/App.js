f2de8eec4ded8082d7f795d5de71d748067f9c9f
Prince Donkor <prince.donkor74@gmail.com>
2019-01-20 13:45:06 -0600

    pulling develop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Main/Dashboard/Dashboard';
 import Main from './components/Main/Main';
+import SignIn from './components/Auth/SignIn';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Main/>
+        {/* <Main/> */}
+        <SignIn />
       </div>
     );
   }
